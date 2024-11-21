e11c73d373314d3a236b5140570f8b17279617e1
jsep <jomchai.suk09@gmail.com>
2024-11-22 00:32:06 +0700

    fix padding

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -14,14 +14,10 @@ const Trending = () => {
     getMoviesItem();
   }, []);
 
-  // useEffect(() => {
-  //   console.log(trendingMovie);
-  // }, [trendingMovie]);
-
   return (
     <div className="flex flex-col bg-slate-950 ">
-      <h1 className="flex py-5  font-bold text-2xl text-white">Trending</h1>
-      <div className="flex overflow-x-scroll pb-5  ">
+      <h1 className="flex pt-5  font-bold text-2xl text-white">Trending</h1>
+      <div className="flex overflow-x-scroll  p-5">
         <div className="flex flex-nowrap">
           {trendingMovie.map((movie, index) => {
             return <MovieCard key={index} item={movie}></MovieCard>;
