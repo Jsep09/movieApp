26204134c42a5f5db4e7c2715db5543cd8173b01
jsep <jomchai.suk09@gmail.com>
2024-11-21 10:30:45 +0700

    Carousel ✅

--- src/components/Popular-Carousel.tsx
+++ src/components/Popular-Carousel.tsx
@@ -3,18 +3,16 @@ import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
 import getPopular from "../service/Popularmovie";
 import { Movie } from "../interface/popularmovie";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const PopularCarousel = () => {
   const [movie, setMovies] = useState<Movie[]>([]);
 
   const getMoviesItem = async () => {
     const result: Movie[] = await getPopular();
-    setMovies(result.slice(0, 5));
+    setMovies(result.slice(0, 7));
   };
-
-  useEffect(() => {
-    console.log(movie);
-  }, [movie]);
 
   useEffect(() => {
     getMoviesItem();
@@ -23,14 +21,16 @@ const PopularCarousel = () => {
   return (
     <Carousel
       indicators={false}
-      animation={"fade"}
-      duration={600}
-      next={(next, active) =>
-        console.log(`we left ${active}, and are now at ${next}`)
-      }
-      prev={(prev, active) =>
-        console.log(`we left ${active}, and are now at ${prev}`)
-      }
+      animation={"slide"}
+      duration={3000}
+      interval={5000}
+      stopAutoPlayOnHover={true}
+      indicatorIconButtonProps={{
+        style: {
+          margin: "5px",
+          padding: "1px", // 1
+        },
+      }}
     >
       {movie.map((movie, index) => (
         <CarouselItem key={index} item={movie} />
@@ -44,7 +44,7 @@ interface CarouselItemProps {
 const CarouselItem = ({ item }: CarouselItemProps) => {
   return (
     <Paper
-      className="sm:h-16 md:h-2/4 lg:h-110 xl:h-128 2xl:h-128 bg-slate-950 "
+      className="relative  sm:h-16 md:h-2/4 lg:h-110 xl:h-128 2xl:h-128 bg-slate-950 mt-3 "
       sx={{ borderRadius: "16px" }}
     >
       <img
@@ -53,8 +53,15 @@ const CarouselItem = ({ item }: CarouselItemProps) => {
         className="w-full sm:h-16 md:h-2/4 lg:h-110 xl:h-128 2xl:h-128 object-cover rounded-md"
       />
 
-      <h2 className="text-white">{item.title}</h2>
-      <p className="text-white">{item.overview}</p>
+      <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black/100 via-transparent to-transparent p-5 rounded-md w-full ">
+        <h2 className="text-white text-2xl font-bold">{item.title}</h2>
+        <div className="hidden lg:block">
+          <p className="text-white text-sm ">{item.overview}</p>
+        </div>
+        <Stack direction="row" className="mt-2">
+          <Button variant="outlined">Explore</Button>
+        </Stack>
+      </div>
     </Paper>
   );
 };
