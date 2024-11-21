6d98119e825cfe26cff4eede37c9e3bd5164c004
jsep <jomchai.suk09@gmail.com>
2024-11-21 10:34:08 +0700

    Remove variable is declared but its value is never read Appbar.tsx

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -7,9 +7,9 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
+
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
+
 import MenuItem from "@mui/material/MenuItem";
 
 import { useNavigate } from "react-router-dom";
@@ -29,23 +29,13 @@ function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   // use navigate to another page
@@ -53,7 +43,6 @@ function ResponsiveAppBar() {
 
   const handleNavigate = (path: string) => {
     navigate(path);
-    handleCloseUserMenu();
   };
 
   return (
