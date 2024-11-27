2d9167026287b4399db45bd76b4112e3292093b7
jsep <jomchai.suk09@gmail.com>
2024-11-27 17:56:31 +0700

    Mobile Castdetail

--- src/pages/Castdetail.tsx
+++ src/pages/Castdetail.tsx
@@ -59,20 +59,6 @@ const Castdetail = () => {
               <h2 className="text-white text-3xl md:text-3xl lg:text-4xl 2xl:text-5xl font-bold inline-block">
                 {personDetail?.name}
               </h2>
-              {/* <span className="text-gray-400 mx-1 ">
-                ({movie.release_date.slice(0, 4)})
-              </span> */}
-              {/* <p className="text-gray-400">
-                {movie.release_date} <>|</> {runtimeFommat()} <span>| </span>
-                {movieDetail?.genres.map((value, index, arr) => {
-                  return (
-                    <span key={index} className="text-gray-400">
-                      {value.name}
-                      {index !== arr.length - 1 && " | "}
-                    </span>
-                  );
-                })}
-              </p> */}
               <div className="my-4">
                 <div className="my-4">
                   <h2 className="text-white text-xl md:text-2xl lg:text-2xl 2xl:text-2xl font-bold inline-block mb-2">
@@ -92,6 +78,44 @@ const Castdetail = () => {
                         <MoviePersonCard key={index} movie={movie} />
                       ))}
                     </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Mobile view */}
+        <div className="block md:hidden max-h-screen">
+          <div className=" flex-col justify-center items-center relative mt-5 z-20 text-white text-3xl font-bold">
+            <div className="flex-1 flex justify-center ">
+              <div className="max-w-60">
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${personDetail?.profile_path}`}
+                  alt=""
+                  className="rounded-xl"
+                />
+              </div>
+            </div>
+            <div>
+              <h2 className="text-white text-4xl font-bold inline-block mt-5">
+                {personDetail?.name}
+              </h2>
+
+              <div className="mt-5">
+                <h2 className="text-white text-xl md:text-2xl lg:text-2xl 2xl:text-2xl font-bold inline-block mb-2">
+                  Biography
+                </h2>
+                <p className="text-gray-400 text-xs">
+                  {personDetail?.biography}
+                </p>
+              </div>
+              <div className="flex flex-col  ">
+                <h1 className=" pt-5  font-bold text-xl text-white">Movie</h1>
+                <div className="flex overflow-x-scroll p-5  ">
+                  <div className="flex flex-nowrap text-sm">
+                    {moviePerson.map((movie, index) => (
+                      <MoviePersonCard key={index} movie={movie} />
+                    ))}
                   </div>
                 </div>
               </div>
